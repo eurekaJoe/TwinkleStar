4f5f8d31fcc6b5e6a21d8a731cf8201a307b9d11
eurekaJoe <1349279359@qq.com>
2022-10-05 17:03:23 +0800

    add fog effect

---
+++ Fog/DepthFog.shader
@@ -0,0 +1,94 @@
+﻿// jave.lin 2020.04.17 - 深度雾 - 代码写法很多可以优化，这里做学习用，便于高可读性
+Shader "Custom/DepthFog" {
+    Properties {
+        [KeywordEnum(VIEWSPACE,WORLDSPACE)] _DIST_TYPE ("Distance type", int) = 0
+        [KeywordEnum(LINEAR,EXP,EXP2)] _FUNC_TYPE ("Calculate Func type", int) = 0
+        _MainTex ("Texture", 2D) = "white" {}
+        _NoiseTex ("NoiseTex", 2D) = "white" {}  // 噪点
+
+        _FogColor ("Fog Color", Color) = (0.5, 0.5, 0.5, 1)
+
+        _Start ("Start", Float) = 0
+        _End ("End", Float) = 100
+
+        _Density ("Density", Range(0, 1)) = 0.3
+    }
+    SubShader {
+        ZWrite Off ZTest Always Cull Off
+        Pass {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+            #pragma multi_compile _DIST_TYPE_VIEWSPACE _DIST_TYPE_WORLDSPACE
+            #pragma multi_compile _FUNC_TYPE_LINEAR _FUNC_TYPE_EXP _FUNC_TYPE_EXP2
+            #include "UnityCG.cginc"
+            struct appdata {
+                float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+                uint id : SV_VertexID;
+            };
+            struct v2f {
+                float4 vertex : SV_POSITION;
+                float2 uv : TEXCOORD0;
+                float3 ray : TEXCOORD1;
+            };
+            sampler2D _CameraDepthTexture;
+            sampler2D _MainTex;
+            float4 _MainTex_ST;
+            fixed4 _FogColor;
+            float4x4 _Ray;
+            float _Start;
+            float _End;
+            float _Density;
+            v2f vert (appdata v) {
+                v2f o;
+                o.vertex = UnityObjectToClipPos(v.vertex);
+                o.uv = v.uv;
+                o.ray = _Ray[v.id].xyz;
+                return o;
+            }
+            fixed4 frag (v2f i) : SV_Target {
+
+                float depth = SAMPLE_DEPTH_TEXTURE(_CameraDepthTexture, i.uv);
+                // return depth;
+
+                // 下面的dist就是我们博客文章中公式的z
+                float dist = 0;
+                // dist：depth to world pos
+                #if _DIST_TYPE_VIEWSPACE
+                // 以view space .z
+                float eyeZ = LinearEyeDepth(depth);
+                dist = eyeZ;
+                
+                #else // _DIST_TYPE_WORLDSPACE
+                // dist：以world space distance
+                float linear01depth = Linear01Depth(depth);
+                // float3 wp = _WorldSpaceCameraPos.xyz + i.ray * linear01depth;
+                dist = length(i.ray * linear01depth);
+
+                #endif
+
+                float factor = 0;
+                #if _FUNC_TYPE_LINEAR
+                // factor = (end-z)/(end-start) = z * (-1/(end-start)) + (end/(end-start))
+                factor = (_End - dist) / (_End - _Start);
+                
+                #elif _FUNC_TYPE_EXP
+                // factor = exp(-density*z)
+                factor = exp(-(_Density * dist));
+
+                #else // _FUNC_TYPE_EXP
+                // factor = exp(-(density*z)^2)
+                factor = exp(-pow(_Density * dist, 2));
+
+                #endif
+
+                factor = saturate(factor);
+
+                return lerp(_FogColor, tex2D(_MainTex, i.uv), factor);
+            }
+            ENDCG
+        }
+    }
+}
+
---
+++ Fog/FogPostEffect.cs
@@ -0,0 +1,199 @@
+﻿using UnityEngine;
+using System.Collections;
+[ExecuteInEditMode]
+public class FogPostEffect : PostEffectsBase
+{
+    public Shader fogShader;
+    private Material fogMaterial = null;
+    public Material material
+    {
+        get
+        {
+            fogMaterial = CheckShaderAndCreateMaterial(fogShader, fogMaterial);
+            return fogMaterial;
+        }
+    }
+
+    private Camera myCamera;
+    public Camera Camera
+    {
+        get
+        {
+            if (myCamera == null)
+            {
+                myCamera = GetComponent<Camera>();
+            }
+            return myCamera;
+        }
+    }
+
+    private Transform myCameraTransform;
+    public Transform cameraTransform
+    {
+        get
+        {
+            if (myCameraTransform == null)
+            {
+                myCameraTransform = Camera.transform;
+            }
+
+            return myCameraTransform;
+        }
+    }
+
+    public enum DistType
+    {
+        VIEWSPACE = 0,
+        WORLDSPACE = 1,
+    }       
+    public enum AccumulateMode
+    {
+        LINEAR = 0,
+        EXP = 1,
+        EXP2 = 2
+    }       
+    // [Range(0.1f, 3.0f)]
+    // public float fogDensity = 1.0f;//雾浓度
+
+    public Color fogColor = Color.white;//雾颜色
+    
+    [Header("Noise")]
+    public Texture noiseTexture;//噪声纹理
+    [Range(0f, 1f)]
+    public float noiseSpX = 0.5f;
+    [Range(0f, 1f)]
+    public float noiseSpY = 0.5f;
+    [Range(0.0f, 0.1f)]
+    public float worldPosScale = 0.05f;
+    
+    [Header("HeightFog")]
+    public float heightStart = 1f;
+    public float heightEnd = 0f;
+    [Range(0.0f, 1.0f)]
+    public float heightDensity = 0.3f;
+    [Range(0.0f, 30.0f)]
+    public float heightNoiseScale = 4.0f;
+    
+    [Header("DepthFog")]
+    public float depthStart = 0f;
+    public float depthEnd = 100f;
+    [Range(0.0f, 0.1f)]
+    public float depthDensity = 0.001f;
+    [Range(0.0f, 30.0f)]
+    public float depthNoiseScale = 4.0f;
+    
+    public DistType distType = 0;
+    public AccumulateMode accumulateMode = 0;
+    
+    [Space(20)]
+    [Range(0.0f, 1.0f)] 
+    public float depthHeightRatio = 1f;
+    
+    void OnEnable()
+    {
+        GetComponent<Camera>().depthTextureMode |= DepthTextureMode.Depth;
+    }
+
+    void OnRenderImage(RenderTexture src, RenderTexture dest)
+    {
+        if (material != null)
+        {
+            Matrix4x4 frustumCornors = Matrix4x4.identity;
+            float fov = Camera.fieldOfView;
+            float near = Camera.nearClipPlane;
+            float far = Camera.farClipPlane;
+            float aspect = Camera.aspect;
+
+            float fovWHalf = fov * 0.5f;
+
+            Vector3 toRight = cameraTransform.right * near * Mathf.Tan (fovWHalf * Mathf.Deg2Rad) * aspect;
+            Vector3 toTop = cameraTransform.up * near * Mathf.Tan (fovWHalf * Mathf.Deg2Rad);
+
+            Vector3 topLeft = (cameraTransform.forward * near - toRight + toTop);
+            float camScale = topLeft.magnitude * far/near;
+
+            topLeft.Normalize();
+            topLeft *= camScale;
+
+            Vector3 topRight = (cameraTransform.forward * near + toRight + toTop);
+            topRight.Normalize();
+            topRight *= camScale;
+
+            Vector3 bottomRight = (cameraTransform.forward * near + toRight - toTop);
+            bottomRight.Normalize();
+            bottomRight *= camScale;
+
+            Vector3 bottomLeft = (cameraTransform.forward * near - toRight - toTop);
+            bottomLeft.Normalize();
+            bottomLeft *= camScale;
+
+            frustumCornors.SetRow(0, bottomLeft);
+            frustumCornors.SetRow(1, bottomRight);
+            frustumCornors.SetRow(2, topRight);
+            frustumCornors.SetRow(3, topLeft);
+
+            material.SetMatrix("_Ray", frustumCornors);
+
+            material.SetFloat("_DepthHeightRatio", depthHeightRatio);
+            material.SetFloat("_DepthDensity", depthDensity);
+            material.SetFloat("_HeightDensity", heightDensity);
+            
+            material.SetColor("_FogColor", fogColor);
+            
+            material.SetFloat("_HeightStart", heightStart);
+            material.SetFloat("_HeightEnd", heightEnd);
+            material.SetFloat("_DepthStart", depthStart);
+            material.SetFloat("_DepthEnd", depthEnd);
+
+            material.SetTexture("_NoiseTex", noiseTexture);
+            material.SetFloat("_NoiseSpX", noiseSpX);
+            material.SetFloat("_NoiseSpY", noiseSpY);
+            material.SetFloat("_DepthNoiseScale", depthNoiseScale);
+            material.SetFloat("_HeightNoiseScale", heightNoiseScale);
+            
+            material.SetFloat("_WorldPosScale", worldPosScale);
+
+            switch ((int)distType)
+            {
+                case 0:
+                    material.EnableKeyword("_DIST_TYPE_VIEWSPACE");
+                    material.DisableKeyword("_DIST_TYPE_WORLDSPACE");
+                    break;
+                case 1:
+                    material.EnableKeyword("_DIST_TYPE_WORLDSPACE");
+                    material.DisableKeyword("_DIST_TYPE_VIEWSPACE");
+                    break;
+                default:
+                    break;
+            }
+            
+            switch ((int)accumulateMode)
+            {
+                case 0:
+                    material.EnableKeyword("_FUNC_TYPE_LINEAR");
+                    material.DisableKeyword("_FUNC_TYPE_EXP");
+                    material.DisableKeyword("_FUNC_TYPE_EXP2");
+                    break;
+                case 1:
+                    material.EnableKeyword("_FUNC_TYPE_EXP");
+                    material.DisableKeyword("_FUNC_TYPE_LINEAR");
+                    material.DisableKeyword("_FUNC_TYPE_EXP2");
+                    break;
+                case 2:
+                    material.EnableKeyword("_FUNC_TYPE_EXP2");
+                    material.DisableKeyword("_FUNC_TYPE_LINEAR");
+                    material.DisableKeyword("_FUNC_TYPE_EXP");
+                    break;
+                default:
+                    break;
+            }
+
+            Graphics.Blit(src, dest, material);
+        }
+        else
+        {
+            Graphics.Blit(src, dest);
+        }
+    }
+}
+
